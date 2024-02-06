5abf02de51371fa63ad931b70e23ab71d516c09a
sonaisrayel <sonai@brightcom.com>
2024-02-06 11:31:12 +0400

    Add bcript to project

---
+++ src/libs/crypto-lib.js
@@ -0,0 +1,10 @@
+import bcrypt from "bcrypt";
+
+
+export const compare = async (password,userParams) => {
+   return  await bcrypt.compare(password, userParams.password);
+}
+
+export const makeHash = async (password) => {
+    return await bcrypt.hash(password, 10);
+}
